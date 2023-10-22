a73b1dd01e172526ac1ac47a0a4f8d9c9fd8b37f
StrawberryDan <DanRhysMorris@gmail.com>
2023-10-22 12:48:31 +0100

    Update tests

--- test/Main.cpp
+++ test/Main.cpp
@@ -15,6 +15,7 @@
 #include "Strawberry/Graphics/Framebuffer.hpp"
 #include "Strawberry/Core/Math/Matrix.hpp"
 #include "Strawberry/Core/Timing/Clock.hpp"
+#include "Strawberry/Graphics/Sampler.hpp"
 
 
 int main()
@@ -37,13 +38,15 @@ int main()
 	Pipeline pipeline = device.Create<Pipeline::Builder>()
 		.WithShaderStage(VK_SHADER_STAGE_VERTEX_BIT, device.Create<ShaderModule>("data/Shaders/Mesh.vert.spirv"))
 		.WithShaderStage(VK_SHADER_STAGE_FRAGMENT_BIT,
-						 device.Create<ShaderModule>("data/Shaders/SolidColor.frag.spirv"))
+						 device.Create<ShaderModule>("data/Shaders/Texture.frag.spirv"))
 		.WithVertexInput(vertexInputDescription())
 		.WithPrimitiveTopology(VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST)
 		.WithViewportSize(Core::Math::Vec2i(1920, 1080))
 		.WithPushConstantRange(VK_SHADER_STAGE_VERTEX_BIT, 16 * sizeof(float), 0)
 		.WithPushConstantRange(VK_SHADER_STAGE_FRAGMENT_BIT, 3 * sizeof(float), 16 * sizeof(float))
-		.WithDescriptorSetLayout(DescriptorSetLayout().WithBinding(VK_DESCRIPTOR_TYPE_SAMPLED_IMAGE, 1, VK_SHADER_STAGE_FRAGMENT_BIT))
+		.WithDescriptorSetLayout(
+			DescriptorSetLayout()
+				.WithBinding(VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, 1, VK_SHADER_STAGE_FRAGMENT_BIT))
 		.Build();
 	Swapchain swapchain = device.Create<Swapchain, const Surface&>(surface, Core::Math::Vec2i(1920, 1080));
 	Queue queue = device.Create<Queue>();
@@ -61,7 +64,24 @@ int main()
 	vertices.Push<Core::Math::Vec3f>(Core::Math::Vec3f(0.5f, 1.0f, 0.0f));
 	buffer.SetData(vertices);
 
+
 	Framebuffer framebuffer = pipeline.Create<Framebuffer>();
+
+
+	auto [size, channels, bytes] = Core::IO::DynamicByteBuffer::FromImage("data/dio.png").Unwrap();
+	Buffer textureBuffer = device.Create<Buffer>(bytes.Size(), VK_BUFFER_USAGE_TRANSFER_SRC_BIT);
+	textureBuffer.SetData(bytes);
+	Image texture = device.Create<Image>(size, VK_FORMAT_R8G8B8A8_SRGB, VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_SAMPLED_BIT);
+	commandBuffer.Begin(true);
+	commandBuffer.CopyBufferToImage(textureBuffer, texture, VK_FORMAT_R8G8B8A8_SRGB);
+	commandBuffer.ImageMemoryBarrier(texture, VK_IMAGE_ASPECT_COLOR_BIT, VK_IMAGE_LAYOUT_GENERAL);
+	commandBuffer.End();
+	queue.Submit(commandBuffer);
+	ImageView textureView = texture.Create<ImageView::Builder>()
+	    .WithType(VK_IMAGE_VIEW_TYPE_2D)
+		.WithFormat(VK_FORMAT_R8G8B8A8_SRGB)
+		.Build();
+	Sampler sampler = device.Create<Sampler>(VK_FILTER_NEAREST, VK_FILTER_NEAREST);
 
 
 	Core::Clock clock;
@@ -84,19 +104,24 @@ int main()
 		Core::Math::Mat4f MVPMatrix;
 		Core::Math::Vec3f Color((std::sinf(*clock) + 1.0f) / 2.0f,
 								(std::cosf(0.25 * *clock) + 1.0f) / 2.0f,
-								(std::cosf(0.5 * *clock + M_PI_2) + 1.0f) / 2.0f);
+								(std::cosf(0.5 * *clock) + 1.0f) / 2.0f);
 
 
-		commandBuffer.Begin(false);
+		pipeline.SetUniformTexture(sampler, textureView, VK_IMAGE_LAYOUT_GENERAL, 0, 0);
+
+
+		commandBuffer.Begin(true);
 		commandBuffer.BeginRenderPass(pipeline, framebuffer);
 		commandBuffer.BindPipeline(pipeline);
 		commandBuffer.BindVertexBuffer(0, buffer);
+		commandBuffer.BindDescriptorSet(pipeline, 0);
 		commandBuffer.PushConstants(pipeline, VK_SHADER_STAGE_VERTEX_BIT, Core::IO::DynamicByteBuffer(MVPMatrix), 0);
 		commandBuffer.PushConstants(pipeline, VK_SHADER_STAGE_FRAGMENT_BIT, Core::IO::DynamicByteBuffer(Color), 64);
 		commandBuffer.Draw(6);
 		commandBuffer.EndRenderPass();
-		commandBuffer.CopyToSwapchain(swapchain, framebuffer.GetColorAttachment(0));
-		commandBuffer.ImageMemoryBarrier(swapchain.GetNextImage(), VK_IMAGE_ASPECT_COLOR_BIT, VK_IMAGE_LAYOUT_PRESENT_SRC_KHR);
+		commandBuffer.CopyImageToSwapchain(framebuffer.GetColorAttachment(0), swapchain);
+		commandBuffer.ImageMemoryBarrier(swapchain.GetNextImage(), VK_IMAGE_ASPECT_COLOR_BIT,
+										 VK_IMAGE_LAYOUT_PRESENT_SRC_KHR);
 		commandBuffer.End();
 		queue.Submit(commandBuffer);
 
