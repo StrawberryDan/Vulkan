20502a9ec5b9c7085d6b0ae113aa150d547481d9
StrawberryDan <DanRhysMorris@gmail.com>
2024-04-11 15:59:50 +0100

    If the user does not specify the multisampling level desired then it will default to single sampling

--- src/Strawberry/Vulkan/GraphicsPipeline.cpp
+++ src/Strawberry/Vulkan/GraphicsPipeline.cpp
@@ -239,6 +239,24 @@ namespace Strawberry::Vulkan
 		}
 
 
+		// Use default multisampling if now specified.
+		if (!mMultisampleStateCreateInfo)
+		{
+			mMultisampleStateCreateInfo = VkPipelineMultisampleStateCreateInfo
+			{
+				.sType = VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO,
+				.pNext = nullptr,
+				.flags = 0,
+				.rasterizationSamples = VK_SAMPLE_COUNT_1_BIT,
+				.sampleShadingEnable =  VK_FALSE,
+				.minSampleShading = 1.0,
+				.pSampleMask = nullptr,
+				.alphaToCoverageEnable = VK_FALSE,
+				.alphaToOneEnable = VK_FALSE,
+			};
+		}
+
+
 		// Create the Pipeline
 		std::vector<VkGraphicsPipelineCreateInfo> createInfos {
 			VkGraphicsPipelineCreateInfo {
